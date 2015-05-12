34e7e7a14666d02f9fa626b8f63e134868f5cac9
Piri-Piri <david.pirih@gmx.de>
2015-05-12 08:04:54 +0200

    unity project files added

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log("Loading Level request for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest () {
+		Debug.Log("Quit requested");
+		Application.Quit(); // Do NOT use it for IOS / Android apps!
+	}
+	
+	
+}
---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 10;
+	
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		
+		NextGuess();
+	}
+	
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+	
+	public void GuessLower () {
+		max = guess;
+		NextGuess ();
+	}
+	
+	void NextGuess () {
+		guess = Random.Range(min, max + 1);
+		text.text = guess.ToString();
+		maxGuessesAllowed--;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
